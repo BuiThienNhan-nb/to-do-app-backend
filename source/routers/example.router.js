17e0eaf1083409f033a5895f40dff1b5b410daf7
Thien Nhan <buithiennhan2016@gmail.com>
2022-06-22 14:25:51 +0700

    chore: update example

--- source/routers/example.router.js
+++ source/routers/example.router.js
@@ -2,6 +2,8 @@ import express from "express";
 import { exampleController } from "../controllers/example.controller.js";
 const router = express.Router();
 
+router.get("/", exampleController.example);
+
 router.get("/example", exampleController.example);
 
 export default router;
